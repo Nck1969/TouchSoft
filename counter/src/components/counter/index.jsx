9848bc5577cc1654e2b7d5f7e780361a1323ab1f
NickKern <nck200069@gmail.com>
2021-11-22 14:04:43 +0300

    Complete counter without color by parity

--- counter/src/components/counter/index.jsx
+++ counter/src/components/counter/index.jsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./Counter.css";
 
 export default function Counter() {
-  return <div>helloS</div>;
+  const [count, setCount] = useState(0);
+  const [parity, setParity] = useState("Введено четное число");
+
+  const onClickMinus = (count) => {
+    if (count > 0) {
+      setCount(count - 1);
+      checkParity(count - 1);
+    }
+  };
+
+  const checkParity = (count) => {
+    count % 2 === 0
+      ? setParity("Введено четное число")
+      : setParity("Введено нечетное число");
+    console.log(count);
+  };
+
+  return (
+    <div>
+      <div className='Screen'>
+        <p>{count}</p>
+      </div>
+      <div className='Parity'>
+        <p>{parity}</p>
+      </div>
+      <div className='Buttons'>
+        <button onClick={() => onClickMinus(count)}>-</button>
+        <button
+          onClick={() => {
+            setCount(0);
+            checkParity(0);
+          }}
+        >
+          Reset
+        </button>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+            checkParity(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
